9a55d860d6b9be29afaa62dd32ae1e1193320fe8
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2022-09-14 09:39:57 +0700

    Add Base Modal

--- client/src/components/modal/BaseModal.jsx
+++ client/src/components/modal/BaseModal.jsx
@@ -4,7 +4,7 @@ import { FaRegTimesCircle } from "react-icons/fa";
 
 const rootModal = document.getElementById("modal-root");
 
-const BaseModal = ({ show, onHideModal, children }) => {
+const BaseModal = ({ show, onHideModal, className, children }) => {
 
   const modalRef = useRef();
 
@@ -13,7 +13,7 @@ const BaseModal = ({ show, onHideModal, children }) => {
       onHideModal();
     }
   };
-ww
+
   useEffect(() => {
     if (show) {
       document.body.style.overflow = "hidden";
@@ -26,13 +26,13 @@ ww
     show &&
     ReactDOM.createPortal(
       <div
-        className="flex items-center justify-center h-full overflow-y-auto w-full bg-black bg-opacity-50 fixed top-0 left-0 z-10"
+        className="flex items-center justify-center h-full overflow-y-auto w-full bg-black bg-opacity-50 fixed top-0 left-0 z-10 "
         ref={modalRef}
         onClick={updateRef}
       >
-        <div className="bg-white rounded-lg p-12 shadow-lg relative w-1/2 max-h-[calc(100vh-210px)] overflow-y-auto">
+        <div className={`bg-white rounded-lg p-12 shadow-lg relative max-h-[calc(100vh-210px)] overflow-y-auto ${className} animate-[pop-up_0.1s_ease-in-out] scale-100`}>
           <FaRegTimesCircle
-            className="absolute right-8 top-8 text-2xl cursor-pointer"
+            className="absolute right-12 top-12 text-2xl cursor-pointer"
             onClick={onHideModal}
           />
           {children}
