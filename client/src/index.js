91adcdcab14b84a38f7a6066802f2f7e87b60035
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2022-09-18 21:07:36 +0700

    tweaks

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,32 @@
-import './assets/css/index.css';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Dashboard from './pages/dashboard';
-import CalendarPage from './pages/calendar';
-import Teams from './pages/teams';
-import People from './pages/teams/people';
-import ProjectDashboard from './pages/teams/projects';
-import Profile from './pages/profile';
-import Settings from './pages/teams/settings';
-import Meeting from './pages/teams/meeting';
-import Login from './pages/login';
-import Register from './pages/register';
+import "./assets/css/index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import CalendarPage from "./pages/calendar";
+import Teams from "./pages/teams";
+import People from "./pages/teams/people";
+import ProjectDashboard from "./pages/teams/projects";
+import Profile from "./pages/profile";
+import Settings from "./pages/teams/settings";
+import Meeting from "./pages/teams/meeting";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Dashboard/>}/>
-            <Route path='login' element={<Login/>}/>
-            <Route path='register' element={<Register/>}/>
-            <Route path='calendar' element={<CalendarPage/>}/>
-            <Route path='profile' element={<Profile/>}/>
-            <Route path='teams' element={<Teams/>}/>
-            <Route path='teams/:teamId' element={<ProjectDashboard/>}/>
-            <Route path='teams/:teamId/projects' element={<ProjectDashboard/>}/>
-            <Route path='teams/:teamId/people' element={<People admin={true}/>}/>
-            <Route path='teams/:teamId/meeting' element={<Meeting/>}/>
-            <Route path='teams/:teamId/settings' element={<Settings/>}/>    
-        </Routes>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+      <Route path="calendar" element={<CalendarPage />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="teams" element={<Teams />} />
+      <Route path="teams/:teamId/projects" element={<ProjectDashboard />} />
+      <Route path="teams/:teamId/people" element={<People admin={true} />} />
+      <Route path="teams/:teamId/meeting" element={<Meeting />} />
+      <Route path="teams/:teamId/settings" element={<Settings />} />
+      <Route path="teams/:teamId" element={<ProjectDashboard />} />
+    </Routes>
+  </BrowserRouter>
 );
--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -1,52 +1,67 @@
+import { Link } from "react-router-dom";
 import CalendarIcon from "../assets/svg/CalendarIcon";
 import DashboardIcon from "../assets/svg/DashboardIcon";
 import KerjaWoiIcon from "../assets/svg/KerjaWoiIcon";
 import LogoutIcon from "../assets/svg/LogoutIcon";
 import TeamIcon from "../assets/svg/TeamIcon";
 
-const DashboardLayout = ({children})=>{
-    return (
-        <div className="lg:grid lg:grid-cols-6 w-100 min-h-screen h-auto">
-            <nav className="bg-dark-purple lg:col-span-1 h-100 flex flex-col justify-between">
-                <div className="flex flex-col p-4">
-                    <a href='/' className="flex flex-row items-center gap-8 mb-8 font-bold ml-3">
-                    <KerjaWoiIcon />
-                    <h4 className="text-white text-xl">KerjaWOI</h4>
-                    </a>
-                    <div className="flex flex-col gap-4">
-                    <a href='/' className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md">
-                        <DashboardIcon />
-                        <p className="text-white">Dashboard</p>
-                    </a>
-                    <a href='/teams' className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md">
-                        <TeamIcon />
-                        <p className="text-white">Teams</p>
-                    </a>
-                    <a href='/calendar' className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md">
-                        <CalendarIcon />
-                        <p className="text-white">Calendar</p>
-                    </a>
-                    <div className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md">
-                        <LogoutIcon />
-                        <p className="text-white">Logout</p>
-                    </div>
-                    </div>
-                </div>
-                <div className="flex flex-row items-center gap-6 p-4 border-t-[1px] border-dark-blue border-opacity-80">
-                    <img
-                    src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2021/08/rubber-duck-stock-fb.jpg"
-                    alt="Profile" 
-                    className="rounded-full aspect-square object-cover w-1/4"
-                    />
-                    <div className="flex flex-col gap-1 text-white w-3/4">
-                    <h4 className="text-md font-semibold">John Doe</h4>
-                    <a href='/profile' className="text-xs font-light cursor-pointer">View Profile</a>
-                    </div>
-                </div>
-                </nav>
-            <div className="lg:col-span-5 p-10 bg-[#F9F9F9]">{children}</div>
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="lg:grid lg:grid-cols-6 w-100 min-h-screen h-auto">
+      <nav className="bg-dark-purple lg:col-span-1 h-100 flex flex-col justify-between">
+        <div className="flex flex-col p-4">
+          <Link
+            to="/"
+            className="flex flex-row items-center gap-8 mb-8 font-bold ml-3"
+          >
+            <KerjaWoiIcon />
+            <h4 className="text-white text-xl">KerjaWOI</h4>
+          </Link>
+          <div className="flex flex-col gap-4">
+            <Link
+              to="/"
+              className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md"
+            >
+              <DashboardIcon />
+              <p className="text-white">Dashboard</p>
+            </Link>
+            <Link
+              to="/teams"
+              className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md"
+            >
+              <TeamIcon />
+              <p className="text-white">Teams</p>
+            </Link>
+            <Link
+              to="/calendar"
+              className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md"
+            >
+              <CalendarIcon />
+              <p className="text-white">Calendar</p>
+            </Link>
+            <div className="flex flex-row items-center gap-6 cursor-pointer bg-dark-blue bg-opacity-0 hover:bg-opacity-100 transition-all p-4 rounded-md">
+              <LogoutIcon />
+              <p className="text-white">Logout</p>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="flex flex-row items-center gap-6 p-4 border-t-[1px] border-dark-blue border-opacity-80">
+          <img
+            src="https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2021/08/rubber-duck-stock-fb.jpg"
+            alt="Profile"
+            className="rounded-full aspect-square object-cover w-1/4"
+          />
+          <div className="flex flex-col gap-1 text-white w-3/4">
+            <h4 className="text-md font-semibold">John Doe</h4>
+            <Link to="/profile" className="text-xs font-light cursor-pointer">
+              View Profile
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <div className="lg:col-span-5 p-10 bg-[#F9F9F9]">{children}</div>
+    </div>
+  );
+};
 
 export default DashboardLayout;
